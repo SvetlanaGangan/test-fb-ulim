1f473ca40e36c77c5ef6fffd77b1b3d95c0dcac0
Adelina <tapesadelina@gmail.com>
2022-05-07 14:17:31 +0300

    modificat

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
+"use strict"
+const ol = "kkk";
+alert(ol);
+// const pass = ---;
+//
+//
+// if(form.pass.value.length < 6) {
+//     alert("Error: Password must contain at least six characters!");
+//     form.pwd1.focus();
+//     return false;
+// }
 
 
-
-
-
-
+// llll
 
 
 
