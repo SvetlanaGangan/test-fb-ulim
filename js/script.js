7b97741d01423313548006e336fa5dd75e82c7ff
Adelina <tapesadelina@gmail.com>
2022-05-10 14:13:22 +0300

    adaugat

--- js/script.js
+++ js/script.js
@@ -1,13 +1,49 @@
 "use strict"
-const ol = "kkk";
-alert(ol);
-// const pass = ---;
+const name = document.querySelector("#name")
+const pass = document.querySelector("#pass")
+const form = document.querySelector("#form")
+const error = document.querySelector("#error")
+
+form.addEventListener('submit', (e)=>{
+    let messages = [];
+    if (name.value.length === "" || name.value.length === null){
+        messages.push("Name is required")
+    }
+    if (messages.length > 0){
+        e.preventDefault()
+        error.innerText = messages.join(', ')
+    }
+    if (name.value.length <= 6){
+        alert("Your name must be at least 6 characters")
+    }
+    if (name.value.length >= 20){
+        alert("Your name must be less than 20 characters")
+    }
+
+    if (pass.value.length === "" || pass.value.length === null){
+        messages.push("Password is required")
+    }
+    if (pass.value.length <= 6){
+        alert("Your password must be at least 6 characters")
+    }
+    if (pass.value.length >= 20){
+        alert("Your password must be less than 20 characters")
+    }
+
+
+})
+
+// const submit = (e) => {
+//     if (pass.value.length === "" || pass.value.length === null){
+//     alert("Enter your password")
+// }
+//     if (pass.value.length <= 6){
+//         alert("Your password must be at least 6 characters")
+//     }
+//     if (pass.value.length >= 20){
+//         alert("Your password must be less than 20 characters")
+//     }
 //
-//
-// if(form.pass.value.length < 6) {
-//     alert("Error: Password must contain at least six characters!");
-//     form.pwd1.focus();
-//     return false;
 // }
 
 
